cb529bb6fe0ae063443e1184eed7ca6cf4c78211
kendall77-code <kendall.hazzard001@albright.edu>
2025-09-07 19:13:50 -0400

    01_01_python_org.py

--- assignments/01_getting_started/01_01_python_org.py
+++ assignments/01_getting_started/01_01_python_org.py
@@ -1,1 +1,9 @@
 #I'm starting my python.org assignment
+#I didn't know that python had in person event
+#I find it interesting that python is used in places like tawin
+#The fact that python can be used with any type of laptop
+#Python can be used on all different devices but is slower on some of them
+#Python is used in the corporate world as a resume
+#The fact that python offers a beginners guide
+#The fact that python has an election or the fact it has a board
+#The fact that python is used for the arts
